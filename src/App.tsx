4cfaa1eb478f5bdcc8e0b897bb4d32386d5b7366
Miguel A <miguelrodrigo249@gmail.com>
2022-07-03 20:23:24 +0200

    add button to app for testing styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.scss';
+import './Components/Button/Button'
+import Button from './Components/Button/Button';
 
 function App() {
   return (
     <div className="App">
-
+      <Button text='1' action={() => {}}/>
     </div>
   );
 }
