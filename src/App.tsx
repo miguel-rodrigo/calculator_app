f264d488ec3a9086c10cb02fe1c7b46549e95438
Miguel A <miguelrodrigo249@gmail.com>
2022-07-03 21:26:25 +0200

    set button layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,42 @@ import Button from './Components/Button/Button';
 function App() {
   return (
     <div className="App">
-      <Button text='1' type="light" action={() => {}}/>
-      <Button text='%' type="dark" action={() => {}}/>
-      <Button text='=' type="main" action={() => {}}/>
+      <div className="button-row">
+        <Button text='%' type="dark" action={() => {}}/>
+        <Button text='CE' type="dark" action={() => {}}/>
+        <Button text='C' type="dark" action={() => {}}/>
+        <Button text='Del' type="dark" action={() => {}}/>
+      </div>
+      <div className="button-row">
+        <Button text='1/x' type="dark" action={() => {}}/>
+        <Button text='x^2' type="dark" action={() => {}}/>
+        <Button text='SQRT' type="dark" action={() => {}}/>
+        <Button text='/' type="dark" action={() => {}}/>
+      </div>
+      <div className="button-row">
+        <Button text='7' type="light" action={() => {}}/>
+        <Button text='8' type="light" action={() => {}}/>
+        <Button text='9' type="light" action={() => {}}/>
+        <Button text='x' type="dark" action={() => {}}/>
+      </div>
+      <div className="button-row">
+        <Button text='4' type="light" action={() => {}}/>
+        <Button text='5' type="light" action={() => {}}/>
+        <Button text='6' type="light" action={() => {}}/>
+        <Button text='-' type="dark" action={() => {}}/>
+      </div>
+      <div className="button-row">
+        <Button text='1' type="light" action={() => {}}/>
+        <Button text='2' type="light" action={() => {}}/>
+        <Button text='3' type="light" action={() => {}}/>
+        <Button text='+' type="dark" action={() => {}}/>
+      </div>
+      <div className="button-row">
+        <Button text='+/-' type="light" action={() => {}}/>
+        <Button text='0' type="light" action={() => {}}/>
+        <Button text=',' type="light" action={() => {}}/>
+        <Button text='=' type="main" action={() => {}}/>
+      </div>
     </div>
   );
 }
