c2732164887856efc3920e21590e5946d9532fe9
Miguel A <miguelrodrigo249@gmail.com>
2022-07-03 20:23:52 +0200

    setup skeleton for button

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,1 +1,16 @@
-import 'Button.scss'
+import './Button.scss'
+
+interface ButtonProps {
+  text: string
+  action: () => void
+}
+
+const Button = function(props: ButtonProps) {
+  return (
+    <div className="button" onClick={props.action}>
+      <span>{props.text}</span>
+    </div>
+  )
+}
+
+export default Button
